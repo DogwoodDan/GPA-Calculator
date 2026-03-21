675f6eab1c38459133718fd1a43e059c48ceea08
DogwoodDan <legodaniel432@gmail.com>
2026-03-21 19:37:46 -0400

    test

---
+++ GPAcalcConstructor.java
@@ -0,0 +1,97 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class GPAcalcConstructor{
+private double classNumber;
+//initializing all the variables these constructors need.
+Scanner scan=new Scanner(System.in);
+private double sum;
+private int classesAdded;
+double GPAtotal=0;
+double creditHours=0.0;
+double creditHourGetter;
+DecimalFormat decForm=new DecimalFormat("0.##");
+private String studentName="Student";
+
+//this is the method that calculated a student's grade.
+    public double GPAcalcConstructor(){
+
+//gets the number of classes the student is calculating.
+        System.out.println("Please enter how many classes this student has taken thus far");
+        classNumber=scan.nextDouble();
+        double CNGet=classNumber;
+
+/*these if-statements are just making the user experience better by showing that something
+is happening*/
+        while (classNumber>=1){
+            if (classNumber==CNGet){
+                System.out.println("\nPlease enter the first class's grade");
+            }
+            if (classNumber==1){
+                System.out.println("\nPlease enter the last class's grade");
+
+            }
+            if(classNumber<CNGet && classNumber!=1){
+                System.out.println("\nPlease enter the next class's grade");
+
+            }
+            classesAdded=scan.nextInt();
+            classNumber--;
+
+// gets the number of credit hours the student has in each class.
+            System.out.println("How many credit hours was the class?");
+            creditHourGetter=scan.nextDouble();
+            creditHours+=creditHourGetter;
+
+/*this adds the appropriate amount of points to their GPA, depending on their grade
+and credit hours*/
+          if(classesAdded>=95){// A+/A
+              GPAtotal+=4*creditHourGetter;
+          }
+          if((classesAdded>=90) && (classesAdded<95)) {// A-
+              GPAtotal += 3.7*creditHourGetter;
+          }
+          if (classesAdded>85 && classesAdded<90){// B+
+              GPAtotal+=3.3*creditHourGetter;
+          }
+          if (classesAdded==85){ // B
+              GPAtotal+=3*creditHourGetter;
+          }
+          if (classesAdded<85 && classesAdded>=80){// B-
+              GPAtotal+=2.7*creditHourGetter;
+          }
+          if (classesAdded<80 && classesAdded>=75){ // C+
+              GPAtotal+=2.3*creditHourGetter;
+          }
+          if (classesAdded>=70 && classesAdded<75){ // C
+              GPAtotal+=2*creditHourGetter;
+          }
+          if (classesAdded>=60 && classesAdded<70){ // D
+              GPAtotal+=1*creditHourGetter;
+          }
+          else{ //F
+              GPAtotal+=0*creditHourGetter;
+          }
+        }
+
+        GPAtotal/=creditHours;
+
+        return GPAtotal;
+    }
+
+//method that prints the current student's GPA. very self-explanatory.
+    public void PrintGPA(){
+
+        System.out.println(studentName+"'s GPA is "+ decForm.format(GPAtotal));
+    }
+
+//sets the current's student's name so it shows up properly in other parts of the program.
+    public void studentNameSetter(){
+        System.out.println("What is this Student's name?");
+        studentName=scan.nextLine();
+    }
+
+//prints the name of the current student
+    public void studentName(){
+        System.out.print(studentName);
+    }
+}
---
+++ GPAcalcDriver.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class GPAcalcDriver{
+    public static void main(String[]args){
+        Boolean program=true;
+        Scanner input = new Scanner(System.in);
+        int userInput=0;
+        GPAcalcConstructor Student1=new GPAcalcConstructor();
+        GPAcalcConstructor Student2=new GPAcalcConstructor();
+        GPAcalcConstructor Student3=new GPAcalcConstructor();
+        GPAcalcConstructor Student4=new GPAcalcConstructor();
+        GPAcalcConstructor Student5=new GPAcalcConstructor();
+        System.out.println("Welcome to the Student GPA Calculator\n" +
+                "This program can hold up to 5 students' information at a time.");
+        Student1.studentNameSetter();
+        Student1.GPAcalcConstructor();
+        Student1.PrintGPA();
+
+       while (program==true) {
+           System.out.println("\nWhat do you want to do?\n" +
+                   "1. View A Student's GPA\n" +
+                   "2. Input/Change A Student's GPA\n" +
+                   "3. Input/Change A Student's Name\n" +
+                   "4. Exit");
+           userInput=input.nextInt();
+           input.nextLine();
+           switch(userInput){
+               case 1:
+                   System.out.print("Which Student do you want to view?\n" +
+                           "1. ");
+                   Student1.studentName();
+                   System.out.print("\n2. ");
+                   Student2.studentName();
+                   System.out.print("\n3. ");
+                   Student3.studentName();
+                   System.out.print("\n4. ");
+                   Student4.studentName();
+                   System.out.print("\n5. ");
+                   Student5.studentName();
+
+
+                   userInput=input.nextInt();
+                   switch(userInput){
+                       case 1:
+                           Student1.PrintGPA();
+                           break;
+                       case 2:
+                           Student2.PrintGPA();
+                           break;
+                       case 3:
+                           Student3.PrintGPA();
+                           break;
+                       case 4:
+                           Student4.PrintGPA();
+                           break;
+                       case 5:
+                           Student5.PrintGPA();
+                           break;
+                       default:
+                           System.out.println("Please enter a number 1-5");
+                   }
+                   break;
+               case 2:
+                   System.out.print("Which Student do you want to edit?\n" +
+                           "1. ");
+                   Student1.studentName();
+                   System.out.print("\n2. ");
+                   Student2.studentName();
+                   System.out.print("\n3. ");
+                   Student3.studentName();
+                   System.out.print("\n4. ");
+                   Student4.studentName();
+                   System.out.print("\n5. ");
+                   Student5.studentName();
+                   userInput=input.nextInt();
+                   switch(userInput){
+                       case 1:
+                           Student1.GPAcalcConstructor();
+                           Student1.PrintGPA();
+                           break;
+                       case 2:
+                           Student2.GPAcalcConstructor();
+                           Student2.PrintGPA();
+                           break;
+                       case 3:
+                           Student3.GPAcalcConstructor();
+                           Student3.PrintGPA();
+                           break;
+                       case 4:
+                           Student4.GPAcalcConstructor();
+                           Student4.PrintGPA();
+                           break;
+                       case 5:
+                           Student5.GPAcalcConstructor();
+                           Student5.PrintGPA();
+                           break;
+                       default:
+                           System.out.println("Please enter a number 1-5");
+                   }
+                   break;
+               case 3:
+                   System.out.print("Which Student do you want to change the name of?\n" +
+                           "1. ");
+                   Student1.studentName();
+                   System.out.print("\n2. ");
+                   Student2.studentName();
+                   System.out.print("\n3. ");
+                   Student3.studentName();
+                   System.out.print("\n4. ");
+                   Student4.studentName();
+                   System.out.print("\n5. ");
+                   Student5.studentName();
+
+                   userInput=input.nextInt();
+
+                   switch(userInput){
+                       case 1:
+                           Student1.studentNameSetter();
+                           break;
+                       case 2:
+                           Student2.studentNameSetter();
+                           break;
+                       case 3:
+                           Student3.studentNameSetter();
+                           break;
+                       case 4:
+                           Student4.studentNameSetter();
+                           break;
+                       case 5:
+                           Student5.studentNameSetter();
+                           break;
+                       default:
+                           System.out.println("Please enter a value between 1-5");
+                   }
+                   break;
+               case 4:
+                   System.exit(0);
+               default:
+                   System.out.println("Please enter a value between 1-4");
+                   break;
+           }
+
+
+       }
+    }
+
+
+}
